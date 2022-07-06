251080235eaed54cfb07d0642c3b8fba306a8249
curso.ads <curso.ads@LAB06-37>
2022-07-06 19:33:44 -0300

    Adicionar arquivos de projeto.

---
+++ Ex1 Dejalma/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1_Dejalma
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+            Console.WriteLine("Contador do 1 ao 100 de números pares");
+
+            int num1, num2;
+
+            num1 = 2;
+            num2 = 100;
+
+            for (; num1 <= num2; num1 = +2)
+            {
+                Console.WriteLine(num1++);
+            }
+
+            Console.ReadKey();
+        }
+	}
+}
